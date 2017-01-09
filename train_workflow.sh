c5846346ba71065a1023353b51842adf20647e0d
TobiasWeis <weis.tobi@googlemail.com>
2017-01-09 19:20:05 +0100

    new net

--- train_workflow.sh
+++ train_workflow.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # get the new image files from the mirror, delete them on the mirror
-scp sarah@192.168.0.191:~/scripts/faces_120/face*.png ./faces_120/ && ssh sarah@192.168.0.191 'rm /home/sarah/scripts/faces_120/face*.png' && ssh sarah@192.168.0.191 'rm /home/sarah/scripts/faces_120/complete*.png'
+scp sarah@192.168.0.191:~/scripts/faces_120/face_*.png ./faces_120/ && ssh sarah@192.168.0.191 'rm /home/sarah/scripts/faces_120/face_*png' 
+#&& ssh sarah@192.168.0.191 'rm /home/sarah/scripts/faces_120/complete*.png'
+
+scp sarah@192.168.0.191:~/scripts/faces_120/complete_*png ./faces_120/ && ssh sarah@192.168.0.191 'rm /home/sarah/scripts/faces_120/complete_*png' 
 
 # classifiy them into preliminary folders
 ./NN_classify_move.py
